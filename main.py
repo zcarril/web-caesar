f4e1f31ac2e580b00588308832f502f6f02a6a10
zcarril <zcarril@slu.edu>
2017-03-27 01:30:52 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+#
+# Copyright 2007 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import webapp2
+import caesar
+import cgi
+
+def build_page(textarea_content):
+    rot_label="<label>Rotate by:</label>"
+    rotation_input="<input type='number' name='rotation'/>"
+
+    message_label="<label>Type a message:</label>"
+    textarea="<textarea name='message'>"+textarea_content+"</textarea>"
+
+    submit="<input type='submit'/>"
+    form=("<form method='post'>"+rot_label+rotation_input+"<br>"+message_label+textarea+"<br>"+submit+"</form>")
+
+    header="<h2>Web Caesar</h2>"
+
+    return header + form
+
+class MainHandler(webapp2.RequestHandler):
+    def get(self):
+        content = build_page("")
+        self.response.write(content)
+
+    def post(self):
+        message=self.request.get("message")
+        rotation=int(self.request.get("rotation"))
+        encrypt=caesar.encrypt(message,rotation)
+        escaped_message=cgi.escape(encrypt)
+        content= build_page(escaped_message)
+        self.response.write(content)
+
+
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
